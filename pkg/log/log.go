1a4f0060a404537503ca8eceb3b8722fe6ff3354
Kujtim Hoxha <kujtimii.h@gmail.com>
2025-07-04 22:51:43 +0200

    chore: small fix

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -3,30 +3,35 @@ package log
 import (
 	"log/slog"
 	"path/filepath"
+	"sync"
 
 	"github.com/charmbracelet/crush/pkg/config"
 
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
+var initOnce sync.Once
+
 func Init(cfg *config.Config) {
-	logRotator := &lumberjack.Logger{
-		Filename:   filepath.Join(cfg.Options.DataDirectory, "logs", "crush.log"),
-		MaxSize:    10,    // Max size in MB
-		MaxBackups: 0,     // Number of backups
-		MaxAge:     30,    // Days
-		Compress:   false, // Enable compression
-	}
+	initOnce.Do(func() {
+		logRotator := &lumberjack.Logger{
+			Filename:   filepath.Join(cfg.Options.DataDirectory, "logs", "crush.log"),
+			MaxSize:    10,    // Max size in MB
+			MaxBackups: 0,     // Number of backups
+			MaxAge:     30,    // Days
+			Compress:   false, // Enable compression
+		}
 
-	level := slog.LevelInfo
-	if cfg.Options.Debug {
-		level = slog.LevelDebug
-	}
+		level := slog.LevelInfo
+		if cfg.Options.Debug {
+			level = slog.LevelDebug
+		}
 
-	logger := slog.NewJSONHandler(logRotator, &slog.HandlerOptions{
-		Level:     level,
-		AddSource: true,
+		logger := slog.NewJSONHandler(logRotator, &slog.HandlerOptions{
+			Level:     level,
+			AddSource: true,
+		})
+
+		slog.SetDefault(slog.New(logger))
 	})
-
-	slog.SetDefault(slog.New(logger))
 }
