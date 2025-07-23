e6186c65b307e201e1a92df1898fe3e497bfae3c
Andrey Nering <andreynering@users.noreply.github.com>
2025-07-23 14:39:00 -0300

    perf(ansiext): grow string builder to optimize allocations

--- internal/ansiext/ansi.go
+++ internal/ansiext/ansi.go
@@ -10,6 +10,7 @@ import (
 // representations to ensure they are displayed correctly in the UI.
 func Escape(content string) string {
 	var sb strings.Builder
+	sb.Grow(len(content))
 	for _, r := range content {
 		switch {
 		case r >= 0 && r <= 0x1f: // Control characters 0x00-0x1F
